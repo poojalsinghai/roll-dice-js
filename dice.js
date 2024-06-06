0787c8e6143ca2d2c2a4f7a1c25c6e73faa958b5
poojalsinghai <148775071+poojalsinghai@users.noreply.github.com>
2024-06-06 18:36:09 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,23 @@
+// DICE ROLLER PROGRAM
+// console.log(`Hello`);
+
+document.getElementById(`click`).onclick = function rollDice(){
+    const numOfDice = document.getElementById(`numOfDice`).value;
+    const diceResult = document.getElementById(`diceResult`);
+    const diceImages = document.getElementById(`diceImages`);
+    console.log(numOfDice);
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="dice${value}.jpg">`);
+    }
+
+    diceResult.textContent = `dice : ${values.join(', ')}`;
+    diceImages.innerHTML = images.join(' ');
+}
+
+
+
